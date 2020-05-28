25bab62d4eaa7679d75a5cb6d22cbace96d0b29c
Peter Harrison <peter.harrison@helicron.net>
2020-05-28 21:51:41 +0100

    Import ask_for_port from serial package

--- hcconfig/interface.py
+++ hcconfig/interface.py
@@ -10,13 +10,7 @@ import click
 import serial
 from serial import Serial
 from serial.tools.list_ports import comports
-
-try:
-    raw_input
-except NameError:
-    # pylint: disable=redefined-builtin,invalid-name
-    raw_input = input  # in python3 it's "raw"
-    unichr = chr
+from serial.tools.miniterm import ask_for_port
 
 
 def suggest_completions(string, completions):
@@ -281,40 +275,6 @@ class Interface(cmd.Cmd):
 
     def complete_parity(self, text, line, begidx, endidx):
         return suggest_completions(text, VALID['parity'])
-
-
-def ask_for_port():
-    """\
-    Show a list of ports and ask the user for a choice. To make selection
-    easier on systems with long device names, also allow the input of an
-    index.
-
-    Extracted from miniterm:
-    # This function is part of pySerial. https://github.com/pyserial/pyserial
-    # (C)2002-2017 Chris Liechti <cliechti@gmx.net>
-    #
-    # SPDX-License-Identifier:    BSD-3-Clause
-
-    """
-    sys.stderr.write('\n--- Available ports:\n')
-    ports = []
-    for n, (port, desc, hwid) in enumerate(sorted(comports()), 1):
-        sys.stderr.write('--- {:2}: {:20} {!r}\n'.format(n, port, desc))
-        ports.append(port)
-    while True:
-        port = raw_input('--- Enter port index or full name: ')
-        try:
-            index = int(port) - 1
-            if not 0 <= index < len(ports):
-                sys.stderr.write('--- Invalid index!\n')
-                continue
-        except ValueError:
-            pass
-        except KeyboardInterrupt:
-            sys.exit(1)
-        else:
-            port = ports[index]
-        return port
 
 
 @click.command()
